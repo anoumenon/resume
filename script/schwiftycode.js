3a28f470f3c529ca69cf9ade63065cb312a91e9a
Jason Burns <jasonb315@protonmail.com>
2021-02-02 20:39:08 -0500

    hide angled ink

--- script/schwiftycode.js
+++ script/schwiftycode.js
@@ -2,4 +2,29 @@
 
 $(document).ready(function() {
     console.log("it works!!");
+
+    var $ink1 = $( '#ink1' );
+    var $ink2 = $( '#ink2' );
+    var $ink3 = $( '#ink3' );
+    var $ink4 = $( '#ink4' );
+    // var $ink5 = $( '#ink5' );
+    var $ink6 = $( '#ink6' );
+    var $ink7 = $( '#ink7' );
+    var $ink8 = $( '#ink8' );
+    var $ink9 = $( '#ink9' );
+
+    function hideAngles(){
+        $ink1.hide();
+        $ink2.hide();
+        $ink3.hide();
+        $ink4.hide();
+        // $ink5.hide();
+        $ink6.hide();
+        $ink7.hide();
+        $ink8.hide();
+        $ink9.hide();
+    }
+
+    hideAngles();
+
         });
